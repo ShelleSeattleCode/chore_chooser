682436f9df68066d74d40bbb13fddc4a245b2466
ShelleSeattleCode <139507703+ShelleSeattleCode@users.noreply.github.com>
2023-10-27 11:43:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
  
  
- let arr = []
+//  let arr = []
 
- console.log(document.getElementById('submit-task'))
-if( document.getElementById('submit-task').value.length == 0){
-    document.getElementById('submit-task').disabled = true
-    document.getElementById('random').disabled = true
-    document.getElementById('message').innerHTML = "Add a task to get started"
-}
-else{
-    document.getElementById('submit-task').disabled = false
-    document.getElementById('random').disabled = false
-    document.getElementById('message').innerHTML = ""
-}
+ let chores =[
+
+    'make bed', 'dishwasher', 'clean kitchen sink', 'clean bathroom sink', 'clean fridge', 'laundry', 'sweep', 'empty roomba', 'empty dustbuster',
+    'run roomba', 'litterbox', 'clean toilet', 'clean tub', 'dust for five minutes', 'read 7 minutes', 'meditate two minutes', 'nails', 'clean bathroom floor', 
+    'books 3 minutes', 'yoga', 'clean kitchen counter', 'clean bathroom counter', 'clean mirror', 'take out compost', 'take out recycling', 'clean under kitchen sink', 
+    'take out litter bag and set up new one', 'gather mail',
+]
+
+let arr =[
+
+    'make bed', 'dishwasher', 'clean kitchen sink', 'clean bathroom sink', 'clean fridge', 'laundry', 'sweep', 'empty roomba', 'empty dustbuster',
+    'run roomba', 'litterbox', 'clean toilet', 'clean tub', 'dust for five minutes', 'read 7 minutes', 'meditate two minutes', 'nails', 'clean bathroom floor', 
+    'books 3 minutes', 'yoga', 'clean kitchen counter', 'clean bathroom counter', 'clean mirror', 'take out compost', 'take out recycling', 'clean under kitchen sink', 
+    'take out litter bag and set up new one', 'gather mail',
+]
+
+//  console.log(document.getElementById('submit-task'))
+
+// if( document.getElementById('submit-task').value.length == 0){
+//     document.getElementById('submit-task').disabled = true
+//     document.getElementById('random').disabled = true
+//     document.getElementById('message').innerHTML = "Add a task to get started"
+// }
+// else{
+//     document.getElementById('submit-task').disabled = false
+//     document.getElementById('random').disabled = false
+//     document.getElementById('message').innerHTML = ""
+// }
+
+
  let add_task = function(){
     let get_input = document.getElementById('add-task').value
 
@@ -39,24 +58,24 @@ else{
  
 
 
-let input = document.getElementById('add-task')
-input.addEventListener("keypress", (event)=>{
-if(event.key === "Enter"){
-    event.preventDefault()
+// let input = document.getElementById('add-task')
+// input.addEventListener("keypress", (event)=>{
+// if(event.key === "Enter"){
+//     event.preventDefault()
 
-    if(input.value.length !== 0){
-        add_task()
-        document.getElementById('message').innerHTML = ""
+//     if(input.value.length !== 0){
+//         add_task()
+//         document.getElementById('message').innerHTML = ""
 
-    }
-    else{
-        document.getElementById('message').innerHTML = "Please add a task to get started"
+//     }
+//     else{
+//         document.getElementById('message').innerHTML = "Please add a task to get started"
 
-    }
+//     }
 
 
-}
-})
+// }
+// })
 
  let done_adding = function(){
     console.log('done adding')
